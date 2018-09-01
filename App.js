e59aa6e74e21171aae4a1b14fb1c7bd2c3cb2d9d
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-01 21:37:24 +0300

    Finish gradients

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import { sampleSize } from "lodash";
 import { LinearGradient } from "expo";
+import color from "tinycolor2";
 
 import allGradients from "./gradients.json";
 
@@ -10,17 +11,65 @@ export default class App extends React.Component {
     gradients: sampleSize(allGradients, 10)
   };
 
+  changeGradients = () => {
+    this.setState({
+      gradients: sampleSize(allGradients, 10)
+    });
+  };
+
   render() {
     const { gradients } = this.state;
     return (
       <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={[styles.text, styles.h1, styles.white]}>
+            Web Gradients
+          </Text>
+          <Text style={[styles.text, styles.white]}>
+            Linear Gradients from webgradients.com
+          </Text>
+        </View>
         <FlatList
           data={gradients}
-          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          onRefresh={this.changeGradients}
+          refreshing={false}
           keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={() => <View style={[styles.seperator]} />}
           renderItem={({ item }) => (
-            <LinearGradient colors={item.colors} style={styles.gradientWrapper}>
-              <Text style={[styles.text]}>{item.name}</Text>
+            <LinearGradient
+              colors={item.colors}
+              style={[styles.gradientWrapper]}
+              start={[0, 0.5]}
+              end={[1, 0.5]}
+            >
+              <Text
+                style={[
+                  styles.text,
+                  styles.gradientName,
+                  color(item.colors[0]).isDark() ? styles.white : {}
+                ]}
+              >
+                {item.name}
+              </Text>
+              <View style={[styles.colorsStringContainer]}>
+                <Text
+                  style={[
+                    styles.text,
+                    color(item.colors[0]).isDark() ? styles.white : {}
+                  ]}
+                >
+                  {item.colors[0]}
+                </Text>
+                <Text
+                  style={[
+                    styles.text,
+                    { marginLeft: 10 },
+                    color(item.colors[0]).isDark() ? styles.white : {}
+                  ]}
+                >
+                  {item.colors[1]}
+                </Text>
+              </View>
             </LinearGradient>
           )}
         />
@@ -32,16 +81,38 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center"
+    backgroundColor: "#241937",
+    justifyContent: "center",
+    paddingHorizontal: 15
+  },
+  header: {
+    paddingVertical: 30,
+    alignItems: "center"
+  },
+  white: {
+    color: "#fff"
+  },
+  h1: {
+    fontSize: 30
   },
   text: {
     backgroundColor: "transparent"
   },
   gradientWrapper: {
-    height: 30
+    height: 70,
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    flexDirection: "row"
   },
-  separator: {
+  seperator: {
     height: 10
+  },
+  gradientName: {
+    fontWeight: "bold"
+  },
+  colorsStringContainer: {
+    flexDirection: "row"
   }
 });
