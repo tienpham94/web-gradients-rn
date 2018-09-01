7b69b69b087d216803af246dda56ed0615bb5a5c
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-01 21:31:11 +0300

    Add styles

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { sampleSize } from "lodash";
+import { LinearGradient } from "expo";
+
+import allGradients from "./gradients.json";
 
 export default class App extends React.Component {
+  state = {
+    gradients: sampleSize(allGradients, 10)
+  };
+
   render() {
+    const { gradients } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        
+        <FlatList
+          data={gradients}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <LinearGradient colors={item.colors} style={styles.gradientWrapper}>
+              <Text style={[styles.text]}>{item.name}</Text>
+            </LinearGradient>
+          )}
+        />
       </View>
     );
   }
@@ -17,8 +32,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center"
   },
+  text: {
+    backgroundColor: "transparent"
+  },
+  gradientWrapper: {
+    height: 30
+  },
+  separator: {
+    height: 10
+  }
 });
